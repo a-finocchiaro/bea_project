1bdcce3be4c0872efc48c21a736728824f897796
Aaron Finocchiaro <afinny10@gmail.com>
2021-07-28 16:41:12 -0700

    master: ran setup script again, updated tar file

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(
-  name = 'pyBea',         # How you named your package folder (MyLib)
+  name = 'bea_data',         # How you named your package folder (MyLib)
   packages = ['bea_data'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
