5a50d28b08d843a80298934c8070aed222112729
Aaron Finocchiaro <afinny10@gmail.com>
2021-07-28 17:17:06 -0700

    updated release version info

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import find_packages
 setup(
   name = 'bea-data',
   packages=find_packages(include=['bea_data']),
-  version = '0.1',
+  version = '1.0',
   license='MIT',
   description = 'A python library that interacts with the Burea of Econmic Analysis API',
   author = 'Aaron Finocchiaro',
   author_email = 'afinny10@gmail.com',
   url = 'https://github.com/a-finocchiaro/bea_project',
-  download_url = 'https://github.com/a-finocchiaro/bea_project/archive/v_01.tar.gz',
+  download_url = 'https://github.com/a-finocchiaro/bea_project/archive/v_1.tar.gz',
   keywords = ['Pandas', 'Economic', 'Analysis', 'Bureau'],
   install_requires=[
           'pandas',
